77fc17d4c25fe07561fc769ee19b84dd0a4629d0
umaid-hassan <hassan.umaid@gmail.com>
2022-08-06 15:41:02 +0200

    up

--- ncdftoexl.R
+++ ncdftoexl.R
@@ -5,3 +5,10 @@ library(RNetCDF)
 
 data <- open.nc("../data files/E5_LR_ERA5.d18Op_timmean.nc")
 print.nc(data)
+
+tp <- var.get.nc(data, "wisoaprt_d")
+
+df <- data.frame(TotalPrecipitation = tp)
+df
+
+write_xlsx(df, '../data files/experiment.xlsx')
